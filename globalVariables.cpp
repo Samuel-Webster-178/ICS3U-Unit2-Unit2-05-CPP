84bc89962bc62c3ed31a7af11ae5e7406c06612f
Samuel-Webster-178 <samuel.webster@mths.ca>
2022-03-08 19:42:23 +0000

    init

---
+++ globalVariables.cpp
@@ -0,0 +1,35 @@
+// Copyright (c) 2022 Samuel Webster All rights reserved
+//
+// Created by Samuel Webster
+// Created on February 2022
+// This program shows how local and global variables work
+
+#include <iostream>
+
+// global variable
+int variableX = 25;
+
+void localVariable() {
+    // this shows what happens with local variables
+    int variableX = 10;
+    int variableY = 30;
+    int variableZ = variableX + variableY;
+    std::cout << "Local variableX, variableY, variableZ: "
+    << variableX << "+" << variableY << "=" << variableZ << std::endl;
+}
+
+void globalVariable() {
+    // this shows what happens with global variables
+    variableX = variableX + 1;
+    int variableY = 30;
+    int variableZ = variableX + variableY;
+
+    std::cout << "Global variableX, variableY, variableZ: "
+        << variableX << "+" << variableY << "=" << variableZ << std::endl;
+}
+
+int main() {
+    // this function calls local and global
+    localVariable();
+    globalVariable();
+}
